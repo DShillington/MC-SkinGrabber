8dfb38ca4b85c6e82fbe86a8999aa18876eac12b
papertazer <devin519@live.ca>
2016-04-15 03:17:21 -0300

    first.
    Put everything on github.

---
+++ MCSkin.js
@@ -0,0 +1,25 @@
+/* 
+ * Script to grab the player skin
+ *-papertazer
+ */
+ 
+function AddEvent(html_element, event_name, event_function) {       
+   if(html_element.attachEvent)
+      html_element.attachEvent("on" + event_name, function() {event_function.call(html_element);}); 
+   else if(html_element.addEventListener)
+      html_element.addEventListener(event_name, event_function, false); 
+}
+ 
+function getSkin() {
+    var input = document.getElementById("input").value;
+    var url = "http://s3.amazonaws.com/MinecraftSkins/" + input + ".png";
+    document.getElementById("actualSkin").src = url;
+}
+ 
+function load() {
+    AddEvent(document.getElementById("btn"), "click", getSkin);
+    AddEvent(document.getElementById("skinGetterForm"), "submit", function() {
+    getSkin();
+    return false;
+    });
+}
